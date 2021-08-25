fbac0edac318d28dc0ace6bf81995fb075eb6ae5
LAPTOP-QCSFF0C1\L B O <ludvigbaumolss@hotmail.se>
2021-08-25 16:48:58 +0200

    Change circumradius value to 6

--- Assets/Scripts/HexSettings.cs
+++ Assets/Scripts/HexSettings.cs
@@ -4,8 +4,8 @@ public static class HexSettings
 {
     //This is the radius of the outer circle that crosses all corenrs,
     //determined by drawing a line form the center of the hex to any corner.
-    //It is used to determine the size of each side of the hex.
-    public const float circumRadius = 10f; 
+    //It is used to determine the size of each side of the virtual hex or the space inbetween the hexes.
+    public const float circumRadius = 6f; 
 
     //This describes the radius of thee inner cricle located in a hexagon
     public const float inRadius = circumRadius * equationValue;
@@ -14,14 +14,4 @@ public static class HexSettings
     //Best described here: https://www.omnicalculator.com/math/hexagon
     //And here  https://www.youtube.com/watch?v=A8YVK7kxZOw
     private const float equationValue = 0.866025404f;
-
-
-    //public static Vector3[] corners = {
-    //    new Vector3(0f, 0f, circumRadius),
-    //    new Vector3(inRadius, 0f, 0.5f * circumRadius),
-    //    new Vector3(inRadius, 0f, -0.5f * circumRadius),
-    //    new Vector3(0f, 0f, -circumRadius),
-    //    new Vector3(-inRadius, 0f, -0.5f * circumRadius),
-    //    new Vector3(-inRadius, 0f, 0.5f * circumRadius)
-    //};
 }
