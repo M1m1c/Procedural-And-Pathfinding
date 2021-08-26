b64657041fccf381a6b8726f95b740b96ca480f0
LAPTOP-QCSFF0C1\L B O <ludvigbaumolss@hotmail.se>
2021-08-26 15:12:49 +0200

    refactored for redability

--- Assets/Scripts/HexGrid.cs
+++ Assets/Scripts/HexGrid.cs
@@ -21,32 +21,35 @@ public class HexGrid : MonoBehaviour
         gridCanvas = GetComponentInChildren<Canvas>();
 
         tiles = new HexTile[height * width];
+        int i = 0;
 
-        for (int y = 0, i = 0; y < height; y++)
+        for (int y = 0; y < height; y++)
         {
             for (int x = 0; x < width; x++)
             {
-                CreateTile(x, y, i++);
+                tiles[i]=CreateTile(x, y);
+                i++;
             }
         }
     }
 
-    void CreateTile(int x, int y, int i)
+    HexTile CreateTile(int x, int y)
     {
         Vector3 position;
         position.x = x * (HexSettings.circumRadius * 1.5f);//(x + y * 0.5f - y / 2) * (HexSettings.inRadius * 2.0f);
         position.y = (y + x * 0.5f - x / 2) * (HexSettings.inRadius * 2.0f);//y *(HexSettings.circumRadius * 1.5f);
         position.z = 0f;
 
-        HexTile tile = tiles[i] = Instantiate(tilePrefab);
+        HexTile tile = Instantiate(tilePrefab);
         tile.transform.SetParent(transform, false);
         tile.transform.localPosition = position;
         tile.coordinates = HexCoordinates.FromOffsetCoordinates(x, y);
 
         Text label = Instantiate(tileLabel);
         label.rectTransform.SetParent(gridCanvas.transform, false);
-        label.rectTransform.anchoredPosition =
-            new Vector2(position.x, position.y);
+        label.rectTransform.anchoredPosition = new Vector2(position.x, position.y);
         label.text = tile.coordinates.ToStringOnSeparateLines();
+
+        return tile;
     }
 }
