2e1e7beab2d42bdfa359e2c4222483d80a5a516e
LAPTOP-QCSFF0C1\L B O <ludvigbaumolss@hotmail.se>
2021-09-01 11:34:35 +0200

    Added constructor with two params

--- Assets/Scripts/Utility/PathNode.cs
+++ Assets/Scripts/Utility/PathNode.cs
@@ -4,4 +4,10 @@ public class PathNode
     public HexTile MyTile;
 
     public HexTile ParentTile;
+
+    public PathNode(HexTile myTile, HexTile parentTile)
+    {
+        MyTile = myTile;
+        ParentTile = parentTile;
+    }
 }
