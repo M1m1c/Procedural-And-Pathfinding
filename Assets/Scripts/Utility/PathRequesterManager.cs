e87753a0bb1bb71bb627b4af61f88d4ee8428389
LAPTOP-QCSFF0C1\L B O <ludvigbaumolss@hotmail.se>
2021-09-02 17:59:56 +0200

    Created PathRequestManager and implemented most of its funcitonallity

---
+++ Assets/Scripts/Utility/PathRequesterManager.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathRequesterManager : MonoBehaviour
+{
+    Queue<PathRequest> PathRequestQueue = new Queue<PathRequest>();
+    PathRequest currentRequest;
+
+    static PathRequesterManager PathRequesterInstance;
+    AStarPathFinder PathFinder;
+
+    bool isProceesingPath = false;
+
+    private void Awake()
+    {
+        PathRequesterInstance = this;
+        PathFinder = GetComponent<AStarPathFinder>();
+    }
+
+    public static void RequestPath(Vector2Int pathStart, Vector2Int pathEnd,Action<List<HexTile>,bool> callBack)
+    {
+        PathRequest newRequest = new PathRequest(pathStart, pathEnd, callBack);
+        PathRequesterInstance.PathRequestQueue.Enqueue(newRequest);
+        PathRequesterInstance.TryProcessNextRequest();
+    }
+
+    private void TryProcessNextRequest()
+    {
+        if (!isProceesingPath && PathRequestQueue.Count > 0)
+        {
+            currentRequest = PathRequestQueue.Dequeue();
+            isProceesingPath = true;
+
+            PathFinder.StartFindPath(currentRequest.pathStart, currentRequest.pathEnd);
+        }
+    }
+
+    public void FinishedProcessingPath(List<HexTile> path, bool wasSucessfull)
+    {
+        currentRequest.callBack(path, wasSucessfull);
+        isProceesingPath = false;
+        PathRequesterInstance.TryProcessNextRequest();
+    }
+
+    struct PathRequest
+    {
+        public Vector2Int pathStart;
+        public Vector2Int pathEnd;
+        public Action<List<HexTile>, bool> callBack;
+
+        public PathRequest(Vector2Int start, Vector2Int end, Action<List<HexTile>, bool> call)
+        {
+            pathStart = start;
+            pathEnd = end;
+            callBack = call;
+        }
+    }
+}
