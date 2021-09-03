2da86fbf1e8e68bdfacd31745cbc630c0da01111
LAPTOP-QCSFF0C1\L B O <ludvigbaumolss@hotmail.se>
2021-09-03 14:51:14 +0200

    added swap function

--- Assets/Scripts/Utility/Heap.cs
+++ Assets/Scripts/Utility/Heap.cs
@@ -1,8 +1,30 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Heap<T>
+public class Heap<T> where T : IHeapItem<T>
 {
-  
+    T[] items;
+    int currentItemCount;
+
+    Heap(int maxHeapSize)
+    {
+        items = new T[maxHeapSize];
+    }
+
+ 
+    void Swap(T itemA, T itemB)
+    {
+        items[itemA.HeapIndex] = itemB;
+        items[itemB.HeapIndex] = itemA;
+
+        int itemAHeapindex = itemA.HeapIndex;
+        itemA.HeapIndex = itemB.HeapIndex;
+        itemB.HeapIndex = itemAHeapindex;
+    }
+}
+
+public interface IHeapItem<T> : IComparable<T>
+{
+    int HeapIndex { get; set; }
 }
