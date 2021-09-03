702c318d4528ab4e24f51ca4f049f04b41827d1a
LAPTOP-QCSFF0C1\L B O <ludvigbaumolss@hotmail.se>
2021-09-03 15:10:42 +0200

    Implemented IHeapItem in HexTile class

--- Assets/Scripts/HexTile.cs
+++ Assets/Scripts/HexTile.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HexTile : MonoBehaviour
+public class HexTile : MonoBehaviour, IHeapItem<HexTile>
 {
     //public HexCoordinates coordinates;
 
-    public Vector2Int Coordinates { get;private set; }
+    public Vector2Int Coordinates { get; private set; }
 
     [EnumFlags]
     public TileTags tileProperties;
@@ -15,15 +15,21 @@ public class HexTile : MonoBehaviour
     public int hCost;
     public int fCost { get { return gCost + hCost; } }
 
+    public int HeapIndex { get; set; }
+
     public HexTile parent;
 
     public GameObject Occupant { get; private set; }
 
     private SpriteRenderer spriteRenderer;
 
-    public void Setup(Vector2Int startCoord)
+    public int CompareTo(HexTile hexTileToCompare)
     {
-        Coordinates = startCoord;
+        var compare = fCost.CompareTo(hexTileToCompare.fCost);
+
+        if (compare == 0) { compare = hCost.CompareTo(hexTileToCompare.hCost); }
+
+        return -compare;
     }
 
     //TODO add special circumstance where player can walk onto tiles with pickups
@@ -39,12 +45,17 @@ public class HexTile : MonoBehaviour
             }
         }
 
-        return retval;   
+        return retval;
     }
 
     public void ChangeTileColor(Color newColor)
     {
         spriteRenderer.color = newColor;
+    }
+
+    public void Setup(Vector2Int startCoord)
+    {
+        Coordinates = startCoord;
     }
 
     private void Awake()
