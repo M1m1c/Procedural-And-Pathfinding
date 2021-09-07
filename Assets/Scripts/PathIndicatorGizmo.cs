6b87398e61b4858e514442caac63dc89e092655b
LAPTOP-QCSFF0C1\L B O <ludvigbaumolss@hotmail.se>
2021-09-07 16:50:32 +0200

    Implemented first iteration of path finder gizmo drawing

---
+++ Assets/Scripts/PathIndicatorGizmo.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathIndicatorGizmo : MonoBehaviour
+{
+    [SerializeField] private Color lineColor = Color.white;
+    private LineRenderer lineRenderer;
+    private List<Vector3> positions = new List<Vector3>();
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.startColor = lineColor;
+        lineRenderer.endColor = lineColor;
+    }
+
+    public void SetupPath(List<HexTile> tiles)
+    {
+        positions.Clear();
+
+        foreach (var item in tiles)
+        {
+            positions.Add(item.transform.position);
+        }
+
+        DrawPath(positions);
+    }
+
+    public void RemovefirstPosition()
+    {
+        if (positions.Count != 0)
+        {
+            positions.RemoveAt(0);
+        }
+        DrawPath(positions);
+    }
+
+    private void DrawPath(List<Vector3> path)
+    {
+
+        if (path.Count == 0)
+        {
+            lineRenderer.positionCount = 0;
+            return;
+        }
+
+        var points = new Vector3[path.Count];
+
+        for (int i = 0; i < points.Length; i++)
+        {
+            points[i] = path[i];
+        }
+        lineRenderer.positionCount = points.Length;
+        lineRenderer.SetPositions(points);
+    }
+}
