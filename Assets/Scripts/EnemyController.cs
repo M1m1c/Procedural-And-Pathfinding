e2db2f822d98f6777c2ef6109a7d898cd91047bd
LAPTOP-QCSFF0C1\L B O <ludvigbaumolss@hotmail.se>
2021-09-08 16:05:20 +0200

    Added OnPlayerWalking and OnPlayerStopping funcitons

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,5 +4,17 @@ using UnityEngine;
 
 public class EnemyController : MovableEntity
 {
-   
+
+    public void OnPlayerWalking()
+    {
+        Debug.Log("player is walking");
+    }
+    public void OnPlayerStopping()
+    {
+
+    }
+    private void GetRandomMoveGoal()
+    {
+        HexGrid.GetRandomWalkableTileWithin(this.transform.position, 5);
+    }
 }
