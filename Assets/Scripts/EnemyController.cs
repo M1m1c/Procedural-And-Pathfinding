a7404c6fd4a04a66d336c28305faf6022e5c58b6
LAPTOP-QCSFF0C1\L B O <ludvigbaumolss@hotmail.se>
2021-09-09 10:57:15 +0200

    Made override for MoveAlongPath function in EnemyController

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -49,11 +49,41 @@ public class EnemyController : MovableEntity
 
         oldPath = path;
         pathGizmo.SetupPath(oldPath, transform.position);
+
+        //if (!isPlayerMoving) { return; }
+        //OnPlayerWalking();
+    }
+
+    protected override IEnumerator MoveAlongPath()
+    {
+        isMoving = true;
+        HexTile goalTile = null;
+        while (oldPath.Count > 0)
+        {
+            if (!isMoving) { break; }
+            if (!isPlayerMoving) { break; }
+            var targetTile = oldPath[0];
+            goalTile = targetTile;
+            yield return StartCoroutine(MoveToTile(targetTile));
+            pathGizmo.RemovefirstPosition();
+
+            if (oldPath.Count == 0) { break; }
+            oldPath.RemoveAt(0);
+        }
+
+        if (goalTile != null)
+        {
+            MyGridPos = goalTile.Coordinates;
+            transform.position = goalTile.transform.position;
+        }
+       
+        isMoving = false;
+        yield return null;
     }
 
     private void CreateNewPath()
     {
-       var goalTile = HexGrid.GetRandomWalkableTileWithin(this.transform.position, 4);
+        var goalTile = HexGrid.GetRandomWalkableTileWithin(this.transform.position, 4);
         PathRequestManager.RequestPath(MyGridPos, goalTile.Coordinates, false, OnPathFound, false);
     }
 }
