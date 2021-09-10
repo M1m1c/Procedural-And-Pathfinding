64b0f20615b2f00e58cab534d01b79852000f3e0
LAPTOP-QCSFF0C1\L B O <ludvigbaumolss@hotmail.se>
2021-09-10 17:39:48 +0200

    Added followsteps used to determine how many move the enemy cna make before giving up pursuit of player

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -24,6 +24,9 @@ public class EnemyController : MovableEntity
 
     private EnemyState myState = EnemyState.Patrolling;
 
+    private int maxFollowSteps = 5;
+    private int currentFollowSteps = 5;
+
     public void OnPlayerStartWalking()
     {
         isPlayerMoving = true;
@@ -51,13 +54,15 @@ public class EnemyController : MovableEntity
         {
             CreateNewPath();
             return;
-        } 
+        }
         StartCoroutine(MoveAlongPath());
     }
 
     public override void OnPathFound(List<HexTile> path, bool succeded)
     {
-        if (!succeded || (myState == EnemyState.Patrolling && path.Count > 4))
+        if (!succeded ||
+            (myState == EnemyState.Patrolling && path.Count > 4) ||
+            (myState == EnemyState.FollowingPlayer && currentFollowSteps == 0))
         {
             followTarget = null;
             myState = EnemyState.Patrolling;
@@ -67,14 +72,14 @@ public class EnemyController : MovableEntity
 
         oldPath = path;
         pathGizmo.SetupPath(oldPath, transform.position);
-        
+
         if (oldPath.Count > 0)
         {
             facingTile = oldPath[0];
             UpdateFieldOfView();
         }
-       
-       
+
+
         if (!isPlayerMoving) { return; }
         OnPlayerStartWalking();
     }
@@ -85,18 +90,21 @@ public class EnemyController : MovableEntity
         HexTile goalTile = null;
         while (oldPath.Count > 0)
         {
+
+
             if (oldPath[0]) { facingTile = oldPath[0]; }
             UpdateFieldOfView();
 
             if (!isMoving) { break; }
             if (!isPlayerMoving) { break; }
-            var targetTile = oldPath[0];        
-            goalTile = targetTile;         
+            var targetTile = oldPath[0];
+            goalTile = targetTile;
 
             yield return StartCoroutine(MoveToTile(targetTile));
 
             pathGizmo.RemovefirstPosition();
-            targetTile.DeOccupyTile(this.gameObject);
+            targetTile.DeOccupyTile(this.gameObject);     
+            
             if (oldPath.Count == 0) { break; }
             oldPath.RemoveAt(0);
         }
@@ -106,7 +114,12 @@ public class EnemyController : MovableEntity
             MyGridPos = goalTile.Coordinates;
             transform.position = goalTile.transform.position;
         }
-       
+
+        if (myState == EnemyState.FollowingPlayer)
+        {
+            currentFollowSteps = Mathf.Clamp(currentFollowSteps - 1, 0, maxFollowSteps);
+        }
+
         isMoving = false;
         yield return null;
     }
@@ -131,16 +144,16 @@ public class EnemyController : MovableEntity
             if (!followTarget) { return; }
             PathRequestManager.RequestPath(MyGridPos, followTarget.MyGridPos, false, OnPathFound, false);
         }
-            
+
     }
 
     private void UpdateFieldOfView()
     {
         var lookTile = facingTile;
-        if (oldPath.Count == 0) { return; }  
+        if (oldPath.Count == 0) { return; }
         if (!lookTile) { lookTile = oldPath[0]; }
         var newFieldOfView = HexGrid.GetFieldOfViewTiles(lookTile, this.transform.position);
-        
+
         if (fieldOfView.Count != 0)
         {
             foreach (var item in fieldOfView)
@@ -148,8 +161,8 @@ public class EnemyController : MovableEntity
                 item.ChangeTileColor(Color.white);
             }
         }
-         
-        if(newFieldOfView.Count != 0)
+
+        if (newFieldOfView.Count != 0)
         {
             newFieldOfView.Add(oldPath[0]);
             foreach (var item in newFieldOfView)
@@ -158,6 +171,8 @@ public class EnemyController : MovableEntity
             }
             fieldOfView = newFieldOfView;
         }
+
+        CheckFieldOfViewForPlayer();
     }
 
     private void FixedUpdate()
@@ -182,6 +197,7 @@ public class EnemyController : MovableEntity
                     myState = EnemyState.FollowingPlayer;
                     followTarget = occupant.GetComponent<MovableEntity>();
 
+                    currentFollowSteps = maxFollowSteps;
                     oldPath.Clear();
                     pathGizmo.SetupPath(oldPath, this.transform.position);
                     OnPlayerRequestingPath();
