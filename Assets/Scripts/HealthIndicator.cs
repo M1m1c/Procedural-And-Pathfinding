0109cf29326a1dbf38b7328c92b185267cfca606
LAPTOP-QCSFF0C1\L B O <ludvigbaumolss@hotmail.se>
2021-09-13 12:03:50 +0200

    Added ChangeHealth function and color varialbes, set color in prefab

--- Assets/Scripts/HealthIndicator.cs
+++ Assets/Scripts/HealthIndicator.cs
@@ -4,5 +4,33 @@ using UnityEngine;
 
 public class HealthIndicator : MonoBehaviour
 {
-    public SpriteRenderer[] HealthSpriteSlots = new SpriteRenderer[3]; 
+    public SpriteRenderer[] HealthSpriteSlots = new SpriteRenderer[3];
+
+    public Color InactiveColor;
+
+    private Color defaultColor;
+
+    private int currentHealth;
+
+    private void Awake()
+    {
+        if (HealthSpriteSlots.Length == 0) { return; }
+        defaultColor = HealthSpriteSlots[0].color;
+
+        currentHealth = HealthSpriteSlots.Length;
+    }
+
+    public void ChangeHealth(bool positiveOrNegative)
+    {
+        var changeValue = positiveOrNegative ? 1 : -1;
+
+        var oldHealth = currentHealth;
+        var indexAdjustment = oldHealth;
+        if (oldHealth > 0) { indexAdjustment--; }
+
+        if (indexAdjustment < 0 || indexAdjustment > HealthSpriteSlots.Length) { return; }
+
+        currentHealth = Mathf.Clamp(currentHealth + changeValue, 0, HealthSpriteSlots.Length);
+        HealthSpriteSlots[oldHealth].color = positiveOrNegative ? defaultColor : InactiveColor;
+    }
 }
