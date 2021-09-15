4d2588a179adf6b514bef5298922712478c6d319
LAPTOP-QCSFF0C1\L B O <ludvigbaumolss@hotmail.se>
2021-09-15 19:41:53 +0200

    Removed setting of isMoving in on player stopping,
    It should only be set in the move along path function

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -36,7 +36,6 @@ public class EnemyController : MovableEntity
     }
     public void OnPlayerStopping()
     {
-        isMoving = false;
         isPlayerMoving = false;  
         OnPlayerSelectionAction();
     }
