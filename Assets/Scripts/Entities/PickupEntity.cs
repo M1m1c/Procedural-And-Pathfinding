aa542926248021eab3acec4fd1affc34d14ddbe1
LAPTOP-QCSFF0C1\L B O <ludvigbaumolss@hotmail.se>
2021-09-17 00:46:26 +0200

    added ability to randomise treassure sprite

--- Assets/Scripts/Entities/PickupEntity.cs
+++ Assets/Scripts/Entities/PickupEntity.cs
@@ -5,10 +5,20 @@ public class PickupEntity : MonoBehaviour
 
     public int CashValue { get; private set; }
 
+    public Sprite TreasureSprite_1;
+    public Sprite TreasureSprite_2;
+
     private void Awake()
     {
         var num = Random.Range(1, 11);
 
         CashValue = num * 10;
+
+        var renderer = this.GetComponent<SpriteRenderer>();
+
+        var rand = Random.Range(0, 2);
+
+        if (rand == 0) { renderer.sprite = TreasureSprite_1; }
+        else { renderer.sprite = TreasureSprite_2; }
     }
 }
