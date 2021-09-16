4be6c0868ebca866d1abdd069ef420bb1697e940
LAPTOP-QCSFF0C1\L B O <ludvigbaumolss@hotmail.se>
2021-09-16 12:23:47 +0200

    added a vlaue to pickusp that gets set on awake

--- Assets/Scripts/Entities/PickupEntity.cs
+++ Assets/Scripts/Entities/PickupEntity.cs
@@ -1,8 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PickupEntity : MonoBehaviour
 {
-  
+
+    public int CashValue { get; private set; }
+
+    private void Awake()
+    {
+        var num = Random.Range(1, 11);
+
+        CashValue = num * 10;
+    }
 }
