2fbf6c0f3486a38585bf3bea3c1e3f2ec6b7ff4a
LAPTOP-QCSFF0C1\L B O <ludvigbaumolss@hotmail.se>
2021-09-16 16:11:10 +0200

    added static properties for the tow values that need to remain during scene loads

--- Assets/Scripts/Utility/PersistentScript.cs
+++ Assets/Scripts/Utility/PersistentScript.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PersistentScript : MonoBehaviour
 {
+    public static int TotalScore { get; set; }
+    public static int CurrentHealth { get; set; }
+
     private void Awake()
     {
         DontDestroyOnLoad(this.gameObject);
